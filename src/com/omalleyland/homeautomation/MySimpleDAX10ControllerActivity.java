28b4180bb457a1b52828290fc5cc8e1980bb02eb
DonnO <DonnOMalley1030@gmail.com>
2012-07-15 14:10:40 -0700

    Added Dim/Bright Functionality as well as a separate layout(main.xml) for landscape

--- src/com/omalleyland/homeautomation/MySimpleDAX10ControllerActivity.java
+++ src/com/omalleyland/homeautomation/MySimpleDAX10ControllerActivity.java
@@ -17,18 +17,30 @@ import android.widget.*;
 public class MySimpleDAX10ControllerActivity extends Activity
 {
 
-		Button entranceOnButton;
-		Button entranceOffButton;
-		Button livingRoomOnButton;
-		Button livingRoomOffButton;
-		Button diningRoomOnButtton;
-		Button diningRoomOffButton;
-		Button kitchenOnButton;
-		Button kitchenOffButton;
-		Button donnsOfficeOnButton;
-		Button donnsOfficeOffButton;
-		Button officeCameraOnButton;
-		Button officeCameraOffButton;
+	Button entranceOnButton;
+	Button entranceOffButton;
+	Button entranceDimButton;
+	Button entranceBrightButton;
+	Button livingRoomOnButton;
+	Button livingRoomOffButton;
+	Button livingRoomDimButton;
+	Button livingRoomBrightButton;
+	Button diningRoomOnButtton;
+	Button diningRoomOffButton;
+	Button diningRoomDimButton;
+	Button diningRoomBrightButton;
+	Button kitchenOnButton;
+	Button kitchenOffButton;
+	Button kitchenDimButton;
+	Button kitchenBrightButton;
+	Button donnsOfficeOnButton;
+	Button donnsOfficeOffButton;
+	Button donnsOfficeDimButton;
+	Button donnsOfficeBrightButton;
+	Button officeCameraOnButton;
+	Button officeCameraOffButton;
+	Button officeCameraDimButton;
+	Button officeCameraBrightButton;
 		
     /** Called when the activity is first created. */
     @Override
@@ -42,36 +54,72 @@ public class MySimpleDAX10ControllerActivity extends Activity
 				
 		entranceOffButton = (Button)findViewById(R.id.entranceOffButton);
 		entranceOffButton.setOnClickListener(new entranceOffClick());
+
+		entranceDimButton = (Button)findViewById(R.id.entranceDimButton);
+		entranceDimButton.setOnClickListener(new entranceDimClick());
+
+		entranceBrightButton = (Button)findViewById(R.id.entranceBrightButton);
+		entranceBrightButton.setOnClickListener(new entranceBrightClick());
 		
 		livingRoomOnButton = (Button)findViewById(R.id.livingRoomOnButton);
 		livingRoomOnButton.setOnClickListener(new livingRoomOnClick());
 		
 		livingRoomOffButton = (Button)findViewById(R.id.livingRoomOffButton);
 		livingRoomOffButton.setOnClickListener(new livingRoomOffClick());
+
+		livingRoomDimButton = (Button)findViewById(R.id.livingRoomDimButton);
+		livingRoomDimButton.setOnClickListener(new livingRoomDimClick());
+
+		livingRoomBrightButton = (Button)findViewById(R.id.livingRoomBrightButton);
+		livingRoomBrightButton.setOnClickListener(new livingRoomBrightClick());
 		
 		diningRoomOnButtton = (Button)findViewById(R.id.diningRoomOnButton);
 		diningRoomOnButtton.setOnClickListener(new diningRoomOnClick());
 		
 		diningRoomOffButton = (Button)findViewById(R.id.diningRoomOffButton);
 		diningRoomOffButton.setOnClickListener(new diningRoomOffClick());
+
+		diningRoomDimButton = (Button)findViewById(R.id.diningRoomDimButton);
+		diningRoomDimButton.setOnClickListener(new diningRoomDimClick());
+
+		diningRoomBrightButton = (Button)findViewById(R.id.diningRoomBrightButton);
+		diningRoomBrightButton.setOnClickListener(new diningRoomBrightClick());
 		
 		kitchenOnButton = (Button)findViewById(R.id.kitchenOnButton);
 		kitchenOnButton.setOnClickListener(new kitchenOnClick());
 		
 		kitchenOffButton = (Button)findViewById(R.id.kitchenOffButton);
 		kitchenOffButton.setOnClickListener(new kitchenOffClick());
+
+		kitchenDimButton = (Button)findViewById(R.id.kitchenDimButton);
+		kitchenDimButton.setOnClickListener(new kitchenDimClick());
+
+		kitchenBrightButton = (Button)findViewById(R.id.kitchenBrightButton);
+		kitchenBrightButton.setOnClickListener(new kitchenBrightClick());
 		
 		donnsOfficeOnButton = (Button)findViewById(R.id.donnsOfficeOnButton);
 		donnsOfficeOnButton.setOnClickListener(new donnsOfficeOnClick());
 		
 		donnsOfficeOffButton = (Button)findViewById(R.id.donnsOfficeOffButton);
 		donnsOfficeOffButton.setOnClickListener(new donnsOfficeOffClick());
+
+		donnsOfficeDimButton = (Button)findViewById(R.id.donnsOfficeDimButton);
+		donnsOfficeDimButton.setOnClickListener(new donnsOfficeDimClick());
+
+		donnsOfficeBrightButton = (Button)findViewById(R.id.donnsOfficeBrightButton);
+		donnsOfficeBrightButton.setOnClickListener(new donnsOfficeBrightClick());
 		
 		officeCameraOnButton = (Button)findViewById(R.id.officeCameraOnButton);
 		officeCameraOnButton.setOnClickListener(new officeCameraOnClick());
 		
 		officeCameraOffButton = (Button)findViewById(R.id.officeCameraOffButton);
 		officeCameraOffButton.setOnClickListener(new officeCameraOffClick()); 
+
+		officeCameraDimButton = (Button)findViewById(R.id.officeCameraDimButton);
+		officeCameraDimButton.setOnClickListener(new officeDimClick());
+
+		officeCameraBrightButton = (Button)findViewById(R.id.officeCameraBrightButton);
+		officeCameraBrightButton.setOnClickListener(new officeBrightClick());
 		
     }
 		
@@ -138,6 +186,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 		} 
 	}
 
+	public class entranceDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "7"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class entranceBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "7"; 
+				String DigitalValue = "2"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
 	public class livingRoomOnClick implements Button.OnClickListener {
 		public void onClick(View v) {
 			try {
@@ -161,6 +241,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 				String RoomName = "A"; 
 				String UnitCode = "0"; 
 				String DigitalValue = "0"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class livingRoomDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "0"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class livingRoomBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "0"; 
+				String DigitalValue = "2"; 
 				URLToParse += RoomName + UnitCode + DigitalValue; 
 				new SendDAX10Command().execute(URLToParse);
 			} 
@@ -202,6 +314,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 		} 
 	}
 
+	public class diningRoomDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "1"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class diningRoomBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "1"; 
+				String DigitalValue = "2"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
 	public class kitchenOnClick implements Button.OnClickListener {
 		public void onClick(View v) {
 			try {
@@ -225,6 +369,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 				String RoomName = "A"; 
 				String UnitCode = "2"; 
 				String DigitalValue = "0"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class kitchenDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "2"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class kitchenBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "A"; 
+				String UnitCode = "2"; 
+				String DigitalValue = "2"; 
 				URLToParse += RoomName + UnitCode + DigitalValue; 
 				new SendDAX10Command().execute(URLToParse);
 			} 
@@ -266,6 +442,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 		} 
 	}
 
+	public class donnsOfficeDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "B"; 
+				String UnitCode = "0"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class donnsOfficeBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "B"; 
+				String UnitCode = "0"; 
+				String DigitalValue = "2"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
 	public class officeCameraOnClick implements Button.OnClickListener {
 		public void onClick(View v) {
 			try {
@@ -290,6 +498,38 @@ public class MySimpleDAX10ControllerActivity extends Activity
 				String UnitCode = "8"; 
 				String DigitalValue = "0"; 
 				URLToParse += RoomName + UnitCode + DigitalValue;
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class officeDimClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "B"; 
+				String UnitCode = "8"; 
+				String DigitalValue = "3"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
+				new SendDAX10Command().execute(URLToParse);
+			} 
+			catch (Exception e) { 
+				Toast.makeText(MySimpleDAX10ControllerActivity.this, "EXCEPTION: \n" + e.getMessage(), Toast.LENGTH_LONG).show(); 
+			}
+		} 
+	}
+
+	public class officeBrightClick implements Button.OnClickListener {
+		public void onClick(View v) {
+			try {
+				String URLToParse = "http://www.omalleyland.com:8080/?";  
+				String RoomName = "B"; 
+				String UnitCode = "8"; 
+				String DigitalValue = "2"; 
+				URLToParse += RoomName + UnitCode + DigitalValue; 
 				new SendDAX10Command().execute(URLToParse);
 			} 
 			catch (Exception e) { 
